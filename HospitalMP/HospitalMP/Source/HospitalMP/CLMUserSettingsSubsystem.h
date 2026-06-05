406ec3bbb04af9ef63f3f47c6d7cfcf4ce8ef569
Henrik Halvorsen Aas <henrikha.aas@gmail.com>
2026-06-05 14:29:12 +0200

    Setup all base for new HospitalMP

---
+++ HospitalMP/HospitalMP/Source/HospitalMP/CLMUserSettingsSubsystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "CLMUserSettingsSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract, Blueprintable)
+class HOSPITALMP_API UCLMUserSettingsSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
---
+++ HospitalMP/HospitalMP/Source/HospitalMP/HospitalMP.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HospitalMP.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HospitalMP, "HospitalMP" );
